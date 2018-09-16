e17859bca54e99d3567eeeedeb283379352ed275
Nelson Chen <crazysim@gmail.com>
2018-09-16 00:06:58 -0700

    Fix build script

--- build-kaniko-win.sh
+++ build-kaniko-win.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-docker run --cap-add=SYS_PTRACE --rm -v $(pwd):/workspace gcr.io/kaniko-project/executor:latest --dockerfile=Dockerfile --context=/workspace --tarPath=/workspace/test.tgz
+docker run --cap-add=SYS_PTRACE --rm -v $(pwd):/workspace gcr.io/kaniko-project/executor:latest --dockerfile=Dockerfile --context=/workspace --tarPath=/workspace/test.tar --destination=test
+docker import test.tar test
+rm test.tar
