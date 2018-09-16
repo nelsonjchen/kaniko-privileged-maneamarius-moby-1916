4e7b05b2ab0d3c9ea5a69f01883e7e5ee3d60366
Nelson Chen <crazysim@gmail.com>
2018-09-15 23:50:29 -0700

    Write tarball out

--- build-kaniko-win.sh
+++ build-kaniko-win.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run --cap-add=SYS_PTRACE --rm -v $(pwd):/workspace gcr.io/kaniko-project/executor:latest --dockerfile=Dockerfile --context=/workspace --no-push --tarPath=/workspace/test.tgz
+docker run --cap-add=SYS_PTRACE --rm -v $(pwd):/workspace gcr.io/kaniko-project/executor:latest --dockerfile=Dockerfile --context=/workspace --tarPath=/workspace/test.tgz
