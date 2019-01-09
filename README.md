9e368380fc1f8cfc0da9b0ab448c8e5b72294739
Nelson Chen <crazysim@gmail.com>
2019-01-09 08:30:34 -0700

    Single Tarball

--- README.md
+++ README.md
@@ -15,5 +15,6 @@ So, perhaps a workaround here is to build with [Kaniko][kaniko]. Kaniko is inten
 run via a `docker run` and does not use a nested Docker. Since it uses `docker
 run`, maybe we can add `-cap-add=SYS_PTRACE`.
 
+I did just that, and the output is here. It's what you get for running this.
 
 [kaniko]:https://github.com/GoogleContainerTools/kaniko
--- build-kaniko-win.sh
+++ build-kaniko-win.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
-docker run --cap-add=SYS_PTRACE --rm -v $(pwd):/workspace gcr.io/kaniko-project/executor:latest --dockerfile=Dockerfile --context=/workspace --tarPath=/workspace/test.tar --destination=test
+docker run --cap-add=SYS_PTRACE --rm -v $(pwd):/workspace gcr.io/kaniko-project/executor:latest --dockerfile=Dockerfile --context=/workspace --tarPath=/workspace/test.tar --destination=test  --single-snapshot
 docker import test.tar test
-rm test.tar
